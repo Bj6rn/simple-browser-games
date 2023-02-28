722d465891f09c71c6def138866790370360b3e4
bj6rn <84383944+Bj6rn@users.noreply.github.com>
2023-02-28 16:53:55 +0100

    added rules and animation

--- monty hall game/script.js
+++ monty hall game/script.js
@@ -1,5 +1,5 @@
-const goat_img = "url('goat.png')";
-const car_img = "url('car.png')";
+const goat_img = "url('goat.svg')";
+const car_img = "url('car.svg')";
 const instructions = document.querySelector("#game-instruction");
 let stats_switched = {cars:0, goats:0};
 let stats_stayed = {cars:0, goats:0};
@@ -8,7 +8,7 @@ let doors_opened = [];
 let gamestate = 0; //gamestate 0: select your door, gamestate 1: stay or switch, gamestate 2: all doors open
 let user_choice;
 
-reset_game();
+start_game();
 
 function update_stats() {
     document.querySelector("#switched-cars").innerHTML = stats_switched.cars;
@@ -30,23 +30,8 @@ function get_random_int(max) {
     return Math.floor(Math.random() * max);
 }
 
-function reset_game() {
-    console.clear();
-    //close all doors & reset background
-    document.querySelectorAll(".door").forEach(element => {
-        element.style.backgroundImage = "none";
-    });
-
-
-    //reset variables
-    doors = [];
-    doors_opened = [];
-    gamestate = 0;
-    user_choice = undefined;
-
-    instructions.innerHTML = "Choose one of the three doors.";
-
-    //choose new winning door
+function start_game() {
+    //put the price behind the doors
     const car_door = get_random_int(3);
     if (car_door == 0) {
         doors = ["car", "goat", "goat"];
@@ -55,6 +40,27 @@ function reset_game() {
     } else {
         doors = ["goat", "goat", "car"];
     }
+
+    instructions.innerHTML = "Choose one of the three doors.";
+}
+
+function reset_game() {
+    //close all doors & reset background
+    document.querySelectorAll(".door > .left, .door > .right").forEach(element => {
+        element.style.animation = "close-door 0.5s linear forwards";
+    });
+    document.querySelectorAll(".door").forEach(element => {
+        element.style.backgroundImage = "none";
+    });
+
+    //reset variables
+    doors = [];
+    doors_opened = [];
+    gamestate = 0;
+    user_choice = undefined;
+
+    //start a new game
+    start_game();
 }
 
 function clicked_door(door_index) {
@@ -63,19 +69,18 @@ function clicked_door(door_index) {
         case 0: //first door choice
             user_choice = door_index;
             selected_element.classList.add("user-choice");
-            //show_hint()
             if (doors[user_choice] == "car") {
                 let goats = [];
-                let temp = JSON.parse(JSON.stringify(doors));
-                goats[0] = temp.indexOf("goat");
+                let temp = JSON.parse(JSON.stringify(doors)); //copy the doors data to a temporary array
+                goats[0] = temp.indexOf("goat"); //save original index from the first goat
                 temp.splice(goats[0], 1, "removed");
-                goats[1] = temp.indexOf("goat");
-                let hint = goats[get_random_int(goats.length)];
+                goats[1] = temp.indexOf("goat"); //save original index from the second goat
+                let hint = goats[get_random_int(goats.length)]; //get a random choice between the two goats
                 open_door(hint);
             } else {
                 let temp = JSON.parse(JSON.stringify(doors));
-                temp.splice(user_choice, 1, "user_choice");
-                let hint = temp.indexOf("goat");
+                temp.splice(user_choice, 1, "user_choice"); //replace the chosen goat in the temporary array
+                let hint = temp.indexOf("goat"); //get the index of the left over goat
                 open_door(hint);
             }
             gamestate = 1;
@@ -83,7 +88,7 @@ function clicked_door(door_index) {
             break;
         case 1: //stay or switch decision
             if (doors_opened.includes(door_index)) {
-                //dont do anything, if do is already open
+                //dont do anything, if door is already open
                 return
             }
             
@@ -110,8 +115,8 @@ function clicked_door(door_index) {
                 }
             }  
 
-            open_door(door_index);
-            for (let i = 0; i < 3; i++) {
+            open_door(door_index); //open the selected door
+            for (let i = 0; i < 3; i++) {   //open the left over door
                 if (!doors_opened.includes(i)) {
                     open_door(i);
                     break;
@@ -129,30 +134,32 @@ function clicked_door(door_index) {
 
 function open_door(door_index) {
     const element = document.querySelector(`#door-${door_index+1} > .door`)
-
+    //put price behind the given door and open it
     switch (doors[door_index]) {
         case "car":
+            element.style.backgroundSize = "contain";
             element.style.backgroundImage = car_img;
             break;
         case "goat":
+            element.style.backgroundSize = "60%";
             element.style.backgroundImage = goat_img;
             break;
     }
+
+    document.querySelector(`#door-${door_index+1} > .door > .left`).style.animation = "open-door 1s linear forwards";
+    document.querySelector(`#door-${door_index+1} > .door > .right`).style.animation = "open-door 1s linear forwards";
 
     doors_opened.push(door_index);
 }
 
 document.querySelector("#door-1").addEventListener("click", event => {
-    console.log("door-1 clicked");
     clicked_door(0);
 })
 
 document.querySelector("#door-2").addEventListener("click", event => {
-    console.log("door-2 clicked");
     clicked_door(1);
 })
 
 document.querySelector("#door-3").addEventListener("click", event => {
-    console.log("door-3 clicked");
     clicked_door(2);
 })
